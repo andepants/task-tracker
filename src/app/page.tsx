e6b46c4efb70c657183b8b6bf2807dddbd274c89
Andrew Heim <andrewsheim@gmail.com>
2023-06-21 00:02:44 -0700

    added user and search params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,37 @@
 "use client";
 import Link from 'next/link'
-import { FormEvent } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
 
-  const insertData = async (e : FormEvent) => {
-    e.preventDefault();
-    const res = await fetch('http://localhost:3000/api/postTask', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title: 'Task One', description: 'This is task one' })
-    })
-  }
+  const[user, setUser] = useState<string>('');
 
-  const readAll = async () => {
-    const res = await fetch('http://localhost:3000/api/getAllTasks', {
-      method: 'GET',
-    })
-    const data = await res.json();
-    console.log(data, 'data from readAll');
+  const handleChange = (e: any) => {
+    setUser(e.target.value);
   }
 
   return (
     <main>
       <h1 className="flex justify-center m-5 p-2 text-4xl font-bold">Task Tracker</h1>
-      <Link href="/tasks" className="flex justify-center">
+      <div className="flex justify-center">
+        <label className="m-2 text-xl font-bold p-2">User</label>
+        <input
+          type="text"
+          placeholder="Bob"
+          className="border-2 border-gray-500 m-2 p-2 rounded"
+          value={user}
+          onChange={handleChange}
+        />
+      </div>
+      <Link
+        href={{
+          pathname: '/tasks',
+          query: { user: user },
+        }}
+        className="flex justify-center"
+      >
         <button className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Taks
+          Take me to my tasks
         </button>
         </Link>
       <Link href="/login" className="flex justify-center">
@@ -36,19 +39,6 @@ export default function Home() {
           Login
         </button>
       </Link>
-      <form className="flex justify-center" onSubmit={(e) => insertData(e)}>
-        <input type="text" placeholder="Title" className="border-2 border-gray-500 m-2 p-2 rounded" />
-        <input type="text" placeholder="Description" className="border-2 border-gray-500 m-2 p-2 rounded" />
-        <input type="text" placeholder="Status" className="border-2 border-gray-500 m-2 p-2 rounded" />
-        <input type="text" placeholder="Due Date" className="border-2 border-gray-500 m-2 p-2 rounded" />
-        <button className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Add Task
-        </button>
-      </form>
-
-
-      <button onClick={insertData} className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounde">Insert into database</button>
-      <button onClick={readAll} className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounde">Read All from DB</button>
     </main>
   )
 }
