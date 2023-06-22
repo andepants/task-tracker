bb34755296ddc236459357feeed06e62d3845e51
Andrew Heim <andrewsheim@gmail.com>
2023-06-21 22:27:06 -0700

    removing console.logs

--- src/app/components/task.tsx
+++ src/app/components/task.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 export default function Task({eachTask, editTask} : {editTask: any, eachTask: any}) {
-  console.log(eachTask, 'eachTask from task.tsx')
   const [modalTitle, setModalTitle] = useState('')
   const [modalDescription, setModalDescription] = useState('')
   const [modalStatus, setModalStatus] = useState('')
