f51bfb4e2ee296588081aaca3a4381797d991efd
Andrew Heim <andrewsheim@gmail.com>
2023-06-22 16:50:44 -0700

    added select dropdown for status, and added sorting tasks

--- src/app/components/task.tsx
+++ src/app/components/task.tsx
@@ -1,11 +1,25 @@
 import { useState } from 'react';
+import Select from 'react-select';
 
 export default function Task({eachTask, editTask, deleteTask} : {editTask: any, eachTask: any, deleteTask: any}) {
-  const [modalTitle, setModalTitle] = useState('')
-  const [modalDescription, setModalDescription] = useState('')
-  const [modalStatus, setModalStatus] = useState('')
-  const [modalDueDate, setModalDueDate] = useState<any>(new Date())
+  const [modalTitle, setModalTitle] = useState(eachTask.title)
+  const [modalDescription, setModalDescription] = useState(eachTask.description)
+  const [modalStatus, setModalStatus] = useState(eachTask.status)
+  const [modalDueDate, setModalDueDate] = useState<any>(eachTask.dueDate)
   const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleModalSelect = (selectedOption: any): void => {
+    setModalStatus(selectedOption);
+  }
+
+  const options : { value: string; label: string; }[] = [
+    { value: 'Not Started', label: 'Not Started' },
+    { value: 'Pending', label: 'Pending' },
+    { value: 'On Hold', label: 'On Hold' },
+    { value: 'Completed', label: 'Completed' },
+    { value: 'Urgent', label: 'Urgent' },
+  ]
+
 
   return (
     <>
@@ -13,7 +27,7 @@ export default function Task({eachTask, editTask, deleteTask} : {editTask: any, 
         <h1 className="border-2 border-gray-500 m-2 p-2 rounded">{eachTask.title}</h1>
         <h1 className="border-2 border-gray-500 m-2 p-2 rounded">{eachTask.description}</h1>
         <h1 className="border-2 border-gray-500 m-2 p-2 rounded">{eachTask.status}</h1>
-        <h1 className="border-2 border-gray-500 m-2 p-2 rounded">{eachTask.dueDate}</h1>
+        <h1 className="border-2 border-gray-500 m-2 p-2 rounded">{eachTask.dueDate.slice(0,10)}</h1>
         <dialog id={eachTask.id} open={isModalOpen}>
           <form onSubmit={(e) => {
                 editTask(e, eachTask.id, modalTitle, modalDescription, modalStatus, modalDueDate)
@@ -21,7 +35,7 @@ export default function Task({eachTask, editTask, deleteTask} : {editTask: any, 
               }}>
             <input type="text" value={modalTitle} onChange={(e) => setModalTitle(e.target.value)} placeholder="Title" className="border-2 border-gray-500 m-2 p-2 rounded" />
             <input type="text" value={modalDescription} onChange={(e) => setModalDescription(e.target.value)} placeholder="Description" className="border-2 border-gray-500 m-2 p-2 rounded" />
-            <input type="text" value={modalStatus} onChange={(e) => setModalStatus(e.target.value)} placeholder="Status" className="border-2 border-gray-500 m-2 p-2 rounded" />
+            <Select options={options} placeholder={modalStatus} value={modalStatus} className="m-2 p-2 rounded" onChange={(e) => handleModalSelect(e.value)}/>
             <input type="text" value={modalDueDate} onChange={(e) => setModalDueDate(e.target.value)} placeholder="Due Date" className="border-2 border-gray-500 m-2 p-2 rounded" />
             <button
               className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -4,8 +4,8 @@ import { FormEvent } from 'react';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Task from '../components/task';
-import { TaskProps } from '../interfaces/tasks';
-
+import Select from 'react-select';
+import { GroupBase } from 'react-select';
 
 export default function Tasks() {
   const searchParams = useSearchParams();
@@ -13,9 +13,28 @@ export default function Tasks() {
   const [allTasks, setAllTasks] = useState([])
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-  const [status, setStatus] = useState('')
+  const [status, setStatus] = useState('Not Started')
   const [dueDate, setDueDate] = useState<any>(new Date())
   const [currentUser, setCurrentUser] = useState<any>('')
+  const [sortBy, setSortBy] = useState({ value: 'title_asc', label: 'Sort By' })
+
+
+  const options : { value: string; label: string; }[] = [
+    { value: 'Not Started', label: 'Not Started' },
+    { value: 'Pending', label: 'Pending' },
+    { value: 'On Hold', label: 'On Hold' },
+    { value: 'Completed', label: 'Completed' },
+    { value: 'Urgent', label: 'Urgent' },
+  ]
+
+  const sortOptions: { value: string; label: string; }[] = [
+    { value: 'title_asc', label: 'Title (A-Z)' },
+    { value: 'title_desc', label: 'Title (Z-A)' },
+    { value: 'status_asc', label: 'Status (A-Z)' },
+    { value: 'status_desc', label: 'Status (Z-A)' },
+    { value: 'due_date_asc', label: 'Due Date (Earliest First)' },
+    { value: 'due_date_desc', label: 'Due Date (Latest First)' }
+  ];
 
   const readAll = async () => {
     console.log('inside read all', currentUser.id)
@@ -84,6 +103,9 @@ export default function Tasks() {
       },
       body: JSON.stringify({ title: title, description: description, status: status, dueDate: dueDate, userId: currentUser.id})
     })
+    setTitle('');
+    setDescription('');
+    setStatus('Not Started');
     readAll();
   }
 
@@ -111,6 +133,80 @@ export default function Tasks() {
     readAll();
   }
 
+  function sortByTitleAtoZ(a: any, b: any) {
+    if (a.title < b.title) return -1;
+    if (a.title > b.title) return 1;
+    return 0;
+  }
+  function sortByTitleZtoA(a: any, b: any) {
+    if (a.title < b.title) return 1;
+    if (a.title > b.title) return -1;
+    return 0;
+  }
+  function sortByStatusAtoZ(a: any, b: any) {
+    if (a.status < b.status) return -1;
+    if (a.status > b.status) return 1;
+    return 0;
+  }
+  function sortByStatusZtoA(a: any, b: any) {
+    if (a.status < b.status) return 1;
+    if (a.status > b.status) return -1;
+    return 0;
+  }
+  function sortByDueDateEarliestFirst(a: any, b: any) {
+    if (a.dueDate < b.dueDate) return -1;
+    if (a.dueDate > b.dueDate) return 1;
+    return 0;
+  }
+  function sortByDueDateLatestFirst(a: any, b: any) {
+    if (a.dueDate < b.dueDate) return 1;
+    if (a.dueDate > b.dueDate) return -1;
+    return 0;
+  }
+
+
+  const handleSelect = (selectedOption: any): void => {
+    console.log(selectedOption, 'selectedOption', typeof selectedOption)
+    setStatus(selectedOption);
+  }
+
+  const handleOptionsSelect = (selectedOption: any): void => {
+    setSortBy(selectedOption);
+  }
+
+  useEffect(() => {
+  const sortTasks = (sortBy: any) => {
+    console.log('inside sortTasks', sortBy)
+    if (sortBy.value === 'title_asc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByTitleAtoZ);
+      setAllTasks(sortedAllTasks);
+    } else if (sortBy.value === 'title_desc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByTitleZtoA);
+      setAllTasks(sortedAllTasks);
+    } else if (sortBy.value === 'status_asc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByStatusAtoZ);
+      setAllTasks(sortedAllTasks);
+    } else if (sortBy.value === 'status_desc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByStatusZtoA);
+      setAllTasks(sortedAllTasks);
+    } else if (sortBy.value === 'dueDate_asc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByDueDateEarliestFirst);
+      setAllTasks(sortedAllTasks);
+    } else if (sortBy.value === 'dueDate_desc') {
+      const sortedAllTasks = [...allTasks];
+      sortedAllTasks.sort(sortByDueDateLatestFirst);
+      setAllTasks(sortedAllTasks);
+    }
+  }
+  sortTasks(sortBy);
+}, [sortBy]);
+
+
 
   return (
     <main>
@@ -119,13 +215,14 @@ export default function Tasks() {
       <form className="flex justify-center" onSubmit={(e) => insertData(e)}>
         <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" className="border-2 border-gray-500 m-2 p-2 rounded" />
         <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" className="border-2 border-gray-500 m-2 p-2 rounded" />
-        <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} placeholder="Status" className="border-2 border-gray-500 m-2 p-2 rounded" />
+        <Select options={options} placeholder={status} value={status} className="m-2 p-2 rounded" onChange={(e) => handleSelect(e.value)}/>
         <input type="text" value={dueDate} onChange={(e) => setDueDate(e.target.value)} placeholder="Due Date" className="border-2 border-gray-500 m-2 p-2 rounded" />
         <button className="bg-blue-500 m-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Add Task
         </button>
       </form>
 
+      <Select options={sortOptions} placeholder={sortBy.label} value={sortBy} className="m-2 p-2 rounded" onChange={(e) => handleOptionsSelect(e)}/>
       {allTasks.map((eachTask : any, index: number) => (
         <Task key={index} eachTask={eachTask} editTask={editTask} deleteTask={deleteTask} />
       ))}
