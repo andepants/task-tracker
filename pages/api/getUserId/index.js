85fb62a3f9ed823cee877b896fc11564198f7fde
Andrew Heim <andrewsheim@gmail.com>
2023-06-23 11:46:56 -0700

    working on postgress nextjs changes

--- pages/api/getUserId/index.js
+++ pages/api/getUserId/index.js
@@ -9,9 +9,14 @@ export default async function handle(req, res) {
         username: req.body.user,
       },
     });
+
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
+
+    // Set the Access-Control-Allow-Origin header to allow all origins
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     res.json(user);
   } catch (error) {
     console.error('Error retrieving user:', error);
